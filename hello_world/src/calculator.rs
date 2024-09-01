0ef5a4802126f357cb3650580b7a55448639b5de
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2024-09-01 08:48:18 +0400

    add calculator

--- hello_world/src/calculator.rs
+++ hello_world/src/calculator.rs
@@ -1,3 +1,9 @@
-pub fn calculate(x: i32, y: i32) -> i32 {
-    return x + y;
+pub fn calculate(x: i32, y: i32, operation: &str) -> i32 {
+    match operation {
+        "+" => x + y,
+        "-" => x - y,
+        "*" => x * y,
+        "/" => x / y,
+        _ => panic!("Invalid Operation {}", operation),
+    }
 }
--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,7 +1,13 @@
-mod say_hello;
 mod calculator;
+mod say_hello;
 fn main() {
     say_hello::say_hello("mahmoud");
-    let x = calculator::calculate(5 ,10);
-    println!("operation result is : {}" , x);
+    let x = calculator::calculate(10, 5, "+");
+    println!("sum operation result is : {}", x);
+    let x = calculator::calculate(10, 5, "-");
+    println!("Subtraction operation result is : {}", x);
+    let x = calculator::calculate(10, 5, "*");
+    println!("Multiplication operation result is : {}", x);
+    let x = calculator::calculate(10, 5, "/");
+    println!("Division operation result is : {}", x);
 }
